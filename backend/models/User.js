a714cad52cba9e24ceb4ed65318c793c2b67476f
Justin Tan <justin.tan@themarket.nz>
2022-08-22 11:20:59 +1200

    fix not updating following of user properly

--- backend/models/User.js
+++ backend/models/User.js
@@ -113,12 +113,12 @@ UserSchema.methods.follow = function(id) {
     this.following = this.following.concat([id]);
   }
 
-  return this.save();
+  return this.update({following: this.following});
 };
 
 UserSchema.methods.unfollow = function(id) {
   this.following.remove(id);
-  return this.save();
+  return this.update({following: this.following});
 };
 
 UserSchema.methods.isFollowing = function(id) {
